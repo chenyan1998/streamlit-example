ff8fb5bb07d270fd587c1bd7025c667fe4939221
chenyan <chen_yan@mymail.sutd.edu.sg>
2021-08-24 06:29:27 +0800

    Import data

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,23 @@
 from collections import namedtuple
-import altair as alt
 import math
-import pandas as pd
 import streamlit as st
+import streamlit.components.v1 as stc
+
+# File Processing Pkgs
+import altair as alt
+import pandas as pd
+import docx2txt
+from PIL import Image 
+from PyPDF2 import PdfFileReader
+import pdfplumber
+import numpy as np
+import os 
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 #start
 """
-# Welcome to Streamlit!
+# Welcome to SIA Data Analysis and Visualization !
 
 Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
 
@@ -15,6 +26,51 @@ forums](https://discuss.streamlit.io).
 
 In the meantime, below is an example of what you can do with just a few lines of code:
 """
+
+def save_uploadedfile(uploadedfile):
+     with open(os.path.join("tempDir",uploadedfile.name),"wb") as f:
+         f.write(uploadedfile.getbuffer())
+     return st.success("Saved File:{} to tempDir".format(uploadedfile.name))
+
+# df = pd.DataFrame(np.random.randn(50, 20),columns=('col %d' % i for i in range(20)))
+# st.dataframe(df)  # Same as st.write(df)
+
+st.subheader("Dataset")
+data_file = st.file_uploader("Upload CSV",type=['csv'])
+if st.button("Process"):
+    if data_file is not None:
+        file_details = {"Filename":data_file.name,"FileType":data_file.type,"FileSize":data_file.size}
+        st.write(file_details)
+        df = pd.read_csv(data_file)
+        st.dataframe(df)
+        df_colx = df.iloc[:,14]
+        # #st.dataframe(df_colx)
+        df_coly1 = df.iloc[:,15]
+        df_coly2 = df.iloc[:,13]
+        df_coly3 = df.iloc[:,12]
+        
+        #st.dataframe(df_coly)
+        save_uploadedfile(data_file)
+        chart_data = pd.DataFrame(df_colx,df_coly1)
+        st.line_chart(chart_data)
+        # st.altair_chart(df_colx,df_coly, use_container_width=True)
+
+        #mul lines in one graph
+        chart_data = pd.DataFrame(df_colx, columns=['FEL', 'frends_zTransformation'])
+        st.line_chart(chart_data)
+        st.area_chart(chart_data)
+        st.bar_chart(chart_data)
+
+        #line chart
+        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+        st.line_chart(chart_data)
+        #area chart
+        chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+        st.area_chart(chart_data)
+        #bar chart
+        chart_data = pd.DataFrame(np.random.randn(50, 3),columns=["a", "b", "c"])
+        st.bar_chart(chart_data)
+
 
 
 with st.echo(code_location='below'):
@@ -37,3 +93,6 @@ with st.echo(code_location='below'):
     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
         .mark_circle(color='#0068c9', opacity=0.5)
         .encode(x='x:Q', y='y:Q'))
+
+
+
